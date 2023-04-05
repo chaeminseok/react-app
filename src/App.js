66701544b0c33f6d37cee90dc8b2c12f5123b9d6
Minseok Chae <117037249+chaeminseok@users.noreply.github.com>
2023-04-05 23:51:51 +0900

    start react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React,  { Component } from 'react';
+import Subject from './components/Subject';
+import NAV from './components/NAV';
+import Article from './components/Article';
 import './App.css';
 
-function App() {
-  return (
+
+
+class App extends Component 
+  {
+    constructor(props){
+      super(props);
+      //state 컴포넌트가 실행될 때 constructor함수 있다면 제일먼저 실행되서 초기화를 담당한다.
+      this.state={
+        subject:{title:"web",sub:" world wide web!"},
+        contents:[
+          {id:1, title:'html', desc:'html is for information'},
+            {id:2, title:'css', desc:'css is for design'},
+            {id:3, title:'JavaScript', desc:'javascript is for interactive'}          
+        ]
+      }
+    }
+    render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+  <Subject title={this.state.subject.title} 
+  sub={this.state.subject.sub}></Subject>
+  <Subject title="React" sub=" world wide web!"></Subject>
+  <NAV data ={this.state.contents}></NAV>
+  <Article title="html" desc="    html is makeup language"></Article>
+
     </div>
   );
-}
+}}
 
 export default App;
