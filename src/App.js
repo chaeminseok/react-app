01ab3536b1dc291d938c45300670d347a5a94e14
Minseok Chae <117037249+chaeminseok@users.noreply.github.com>
2023-04-07 23:50:24 +0900

    making event

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ class App extends Component
       super(props);
       //state 컴포넌트가 실행될 때 constructor함수 있다면 제일먼저 실행되서 초기화를 담당한다.
       this.state={
+        mode:'read',
         subject:{title:"web",sub:" world wide web!"},
+        welcome:{title:'welcome',desc:'hello, react!!'},
         contents:[
           {id:1, title:'html', desc:'html is for information'},
             {id:2, title:'css', desc:'css is for design'},
@@ -21,14 +23,33 @@ class App extends Component
       }
     }
     render(){
+      console.log('App render')
+      var _title, _desc = null;
+      if(this.state.mode ==='welcome'){
+        _title = this.state.welcome.title;
+        _desc = this.state.welcome.desc;
+      }else if (this.state.mode ==='read'){
+        _title=this.state.contents[0].title;
+        _desc=this.state.contents[0].desc;
+      }
     return (
     <div className="App">
     
-  <Subject title={this.state.subject.title} 
-  sub={this.state.subject.sub}></Subject>
-  <Subject title="React" sub=" world wide web!"></Subject>
-  <NAV data ={this.state.contents}></NAV>
-  <Article title="html" desc="    html is makeup language"></Article>
+   <Subject 
+  title={this.state.subject.title} 
+  sub={this.state.subject.sub}
+  onChangePage={function(){
+   this.setState({mode:'welcome'});
+  }.bind(this)}>
+  </Subject> 
+
+  <NAV 
+  onChangePage={function(){
+    this.setState({mode:'read'});
+  }.bind(this)} 
+  data ={this.state.contents}
+  ></NAV>
+  <Article title={_title} desc={_desc}></Article>
 
     </div>
   );
